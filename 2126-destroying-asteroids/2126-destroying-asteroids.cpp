eaef0b6150f5d6d91d7dc3d7a4e3ced55faa8c87
TrigunDev <triguntamragouri@gmail.com>
2026-05-31 09:25:59 +0530

    Time: 29 ms (72.55%), Space: 106.6 MB (81.61%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long newMass = mass;
+        int n = asteroids.size();
+        sort(asteroids.begin(),asteroids.end());
+   
+        for(int i = 0; i < n; i++) {
+            if(newMass < asteroids[i]) {
+                return false;
+            }
+
+            newMass += asteroids[i];
+        }
+
+        return true;
+    }
+};
