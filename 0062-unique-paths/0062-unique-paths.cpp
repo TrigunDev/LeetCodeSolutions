1c5f54886d12420d8cdddddc6e0ab8e36516f926
TrigunDev <triguntamragouri@gmail.com>
2026-06-04 09:47:13 +0530

    Time: 0 ms (100%), Space: 8.3 MB (75.13%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> result(n, 1);
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                result[j] += result[j-1];
+            }
+        }
+        return result[n-1];
+    }
+};
